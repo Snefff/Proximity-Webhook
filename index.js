3c518093c9cd82058ce2df4b673c2832e812b0b7
Snef <38905368+Snefff@users.noreply.github.com>
2020-01-16 11:54:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,13 +14,26 @@ server.use(bodyParser.json());
 
 // entry point
 server.post('/example', function (request, response) {
+    var intent = request.body.intent.name
     var param = request.body.intent.inputs;
+    console.log("Intent found: "+intent);
     console.log("List of your entities : ")
     Object.keys(param).forEach(element => { console.log(element + " - " + param[element])});
     response.setHeader('Content-Type', 'application/json');
     response.send(JSON.stringify({
         "speech": "Hello from /example :)",
-        "posts": []
+        "posts": [{
+        "type": "card",
+        "title": "Le titre",
+        "image": "Url de l'image ,
+        "text": "Le texte",
+        "buttons": [{
+                       "openInPanel": false,
+                       "type": "link", //type peut être "lien", ou "button"
+                       "text": "Texte du lien",
+                       "value": "Url du lien" // ou message envoyé au bot dans le cas d'un bouton
+                    }]
+    }]
     }));
 })
 
